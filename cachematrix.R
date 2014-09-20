cb9541ac6c29575a4fcc305149f5030f1c37dd4c
Rajesh Katikam <rajesh.katikam@gmail.com>
2014-09-20 23:33:49 +0530

    fixed the function names and improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,16 @@
 ##makeCacheMatrix- is used to create a matrix. 
-##On succesful execution defines the methods set,get,setInv and getInv. 
+##On succesful execution defines the methods set,get,setInverse and getInverse. 
 ##Intializes inverse(i) to NULL
 ##cacheSolve returns the inverse if already cached.
 ##Else computes, caches  and returns
 
 
 ##At the matrix creation time inverse(i) is intialized to NULL,set and get
-##function are defined
+##functions are defined
 ##get() simply returns the current matrix contents,
-##set after setting the matrix contents to the passed in matrix,
-##resets invers(i) to NULL
-##setInv and getInV set and get the inverse (i). 
-##Also the list of functions is stored in the list
+##set() sets the the matrix contents to the passed in matrix and resets i to NULL,
+##setInverse() and getInVerse() set and get the inverse (i). 
+##Also the list of functions are stored in the list
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -20,19 +19,20 @@ makeCacheMatrix <- function(x = matrix()) {
     i <<- NULL
   }
   get <- function() x
-  setInv <- function(inv) i<<- inv
-  getInv <- function() i
+  setInverse <- function(inv) i<<- inv
+  getInverse <- function() i
   list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
+       setInverse = setInverse,
+       getInverse = getInverse)
+       
 }
 
-##If the inv(i) is not NULL returns the value in i
-##If the inv(i) is NULL compute the inverse and cache it into i.
+##If the inverse(i) is not NULL return the value in i
+##If the inverse(i) is NULL compute the inverse and cache it into the variable i.
 
 cacheSolve <- function(x, ...) {
   ## get i 
-  i <- x$getInv()
+  i <- x$getInverse()
   ##i is not NULL so return the value in i
   if(!is.null(i)) {
     ##This message is printed whenever returning from cache
@@ -43,6 +43,6 @@ cacheSolve <- function(x, ...) {
   ##cache it and return the inverse in i
   data <- x$get()
   i <- solve(data, ...)
-  x$setInv(i)
+  x$setInverse(i)
   i
 }
